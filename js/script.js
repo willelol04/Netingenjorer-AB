2af6c627ca17245c5b1249d3cbac7a9fd3b947b0
willelol04 <147752701+willelol04@users.noreply.github.com>
2024-04-19 16:04:38 +0200

    Adjust departments.html to make it able to retain more information

--- js/script.js
+++ js/script.js
@@ -1,17 +1,232 @@
-let departments = document.querySelectorAll(".department");
-let texts = {
-    "heading": ["CAD/BERÄKNING", "WEBB/BILD", "LÖN", "LAGER OCH INKÖP", "RECEPTION"],
-    "text": ["0", "1", "2", "3", "4"]
+let divDepartments = document.querySelectorAll(".department");
+
+
+const DEPARTMENTS = {
+    0: 
+    {
+        "heading" : "CAD",
+        "function" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "hardware" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "software" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "amountOfEmployees": 4,
+        EMPLOYEES : {
+            0: {
+                firstName : "Ahmed",
+                img : "../img/employees/ahmed.jpg",
+                title : "CAD-ritare"
+    
+            },
+            1: {
+                firstName : "Christina",
+                img : "../img/employees/christina.jpg",
+                title : "CAD-ritare"
+            },
+            2: {
+                firstName : "Jakob",
+                img : "../img/employees/jakob.jpg",
+                title : "Projektledare"
+            },
+            3: {
+                firstName : "Charles",
+                img : "../img/employees/charles.jpg",
+                title : "Konstruktör"
+            }
+        }
+
+    },
+    1: {
+        "heading" : "WEB",
+        "function" : "<qweqweqweqweqweqwe>",
+        "hardware" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "software" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "amountOfEmployees": 3,
+        EMPLOYEES : {
+            0: {
+                firstName : "Ahmed",
+                img : "../img/employees/ahmed.jpg",
+                title : "CAD-ritare"
+    
+            },
+            1: {
+                firstName : "Christina",
+                img : "../img/employees/christina.jpg",
+                title : "CAD-ritare"
+            }
+        }
+    },
+    2: {
+        "heading" : "SALARY",
+        "function" : "<qweqweqweqweqweqwe>",
+        "hardware" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "software" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "amountOfEmployees": 2,
+        EMPLOYEES : {
+            0: {
+                firstName : "Ahmed",
+                img : "../img/employees/ahmed.jpg",
+                title : "CAD-ritare"
+    
+            },
+            1: {
+                firstName : "Christina",
+                img : "../img/employees/christina.jpg",
+                title : "CAD-ritare"
+            }
+        }
+    },
+    3: {
+        "heading" : "INVENTORY",
+        "function" : "<qweqweqweqweqweqwe>",
+        "hardware" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "software" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "amountOfEmployees": 2,
+        EMPLOYEES : {
+            0: {
+                firstName : "Ahmed",
+                img : "../img/employees/ahmed.jpg",
+                title : "CAD-ritare"
+    
+            },
+            1: {
+                firstName : "Christina",
+                img : "../img/employees/christina.jpg",
+                title : "CAD-ritare"
+            }
+        }
+    },
+    4: {
+        "heading" : "RECEPTION",
+        "function" : "<qweqweqweqweqweqwe>",
+        "hardware" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "software" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur, corrupti pariatur incidunt placeat cupiditate magnam iste earum doloremque ipsa sint esse ex. Consequuntur voluptatibus saepe adipisci optio illum maiores.",
+        "amountOfEmployees": 2,
+        EMPLOYEES : {
+            0: {
+                firstName : "Ahmed",
+                img : "../img/employees/ahmed.jpg",
+                title : "CAD-ritare"
+    
+            },
+            1: {
+                firstName : "Christina",
+                img : "../img/employees/christina.jpg",
+                title : "CAD-ritare"
+            }
+        }
+    }
 }
-let text = document.querySelector(".departments-text");
-let heading = document.querySelector(".departments-heading");
+let textGuide = document.querySelector(".department-text-guide");
+
+
+let depHeading = document.querySelector(".department-heading");
+let depEmployees = document.querySelector(".department-employees");
+let depInfo = document.querySelector(".department-info");
+
+function clearDepartmentEmployees() {
+    while(depEmployees.firstChild) {
+        depEmployees.firstChild.remove();
+    }
+}
+
+function addEmployees(departmentInd) {
+    clearDepartmentEmployees();
+
+    depEmployeesHeading = document.createElement("h3");
+
+
+    for(let n = 0; n < DEPARTMENTS[departmentInd].amountOfEmployees; n++) {
+        let empImg = document.createElement("img");
+        let empName = document.createElement("p");
+        let empTitle = document.createElement("p");
+        empDiv = document.createElement("div");
+        empDiv.className = "employee";
+        empImg.src = DEPARTMENTS[departmentInd].EMPLOYEES[n].img;
+        empName.innerText = DEPARTMENTS[departmentInd].EMPLOYEES[n].firstName;
+        empTitle.innerText = DEPARTMENTS[departmentInd].EMPLOYEES[n].title;
+        empDiv.appendChild(empImg);
+        empDiv.appendChild(empName);
+        empDiv.appendChild(empTitle);
+
+        depEmployees.appendChild(empDiv);
+    }
+}
+
+function addDepartmentInfo(departmentInd) {
+
+    while(depInfo.firstChild) {
+        depInfo.firstChild.remove();
+    }
+    
+    let headingFunction = document.createElement("h3");
+    headingFunction.innerText = "Det vi gör:";
+    let textFunction = document.createElement("p");
+    textFunction.innerText = DEPARTMENTS[departmentInd].function;
+
+    depInfo.appendChild(headingFunction);
+    depInfo.appendChild(textFunction);
+
+
+    let headingHardware = document.createElement("h3");
+    headingHardware.innerText = "Hårdvara:";
+    let textHardware = document.createElement("p");
+    textHardware.innerText = DEPARTMENTS[departmentInd].function;
+
+    depInfo.appendChild(headingHardware);
+    depInfo.appendChild(textHardware);
+
+
+    let headingSoftware = document.createElement("h3");
+    headingSoftware.innerText = "Mjukvara:";
+    let textSoftware = document.createElement("p");
+    textSoftware.innerText = DEPARTMENTS[departmentInd].function;
+
+    depInfo.appendChild(headingSoftware);
+    depInfo.appendChild(textSoftware);
+}
+
+function loadDepartmentDiv() {
+    let initDiv = document.querySelector(".department-about");
+
+
+    if(!initDiv) {
+        depAboutDiv = document.createElement("div");
+        depAboutDiv.className = "department-about";
+
+        depEmployees = document.createElement("div");
+        depEmployees.className = "department-employees";
+
+        depEmployeesHeading = document.createElement("div");
+        depEmployeesHeading.innerText = "Anställda";
+        depEmployeesHeading.className = "department-employees-heading";
+
+        depEmployees.appendChild(depEmployeesHeading);
+
+        depInfo = document.createElement("div");
+        depInfo.className = "department-info";
+
+        depAboutDiv.appendChild(depEmployees);
+        depAboutDiv.appendChild(depInfo);
+
+        
+
+    }
+
+    let container = document.querySelector(".container");
+
+    container.appendChild(depAboutDiv);
+
+}
 
 
 
-departments.forEach((dep) => {
+
+
+
+
+divDepartments.forEach((dep) => {
     dep.addEventListener("click", () => {
 
-        departments.forEach((dep) => {
+        divDepartments.forEach((dep) => {
             dep.style.backgroundColor = "#1A1C20";
             dep.style.border = "1px solid white";
             dep.style.color = "white";
@@ -20,10 +235,15 @@ departments.forEach((dep) => {
         dep.style.border = "1px solid black";
         dep.style.color = "black";
 
-        for(let i = 0; i < departments.length; i++) {
-            if(departments[i] === dep) {
-                heading.innerText = texts.heading[i];
-                text.innerText = texts.text[i];
+        
+        for(let i = 0; i < divDepartments.length; i++) {
+            if(divDepartments[i] === dep) {
+                textGuide.remove();
+                loadDepartmentDiv();
+                //console.log(DEPARTMENTS[i]);
+                depHeading.innerHTML = DEPARTMENTS[i].heading;
+                addEmployees(i);
+                addDepartmentInfo(i);
                 break;
 
             }
